bbbc93ba892dc35857f9bd50b90763168d49b224
Kazuki Tobita <kazukiti201@gmail.com>
2018-04-03 02:38:18 +0900

    Create env file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const twitter = require('twitter')
+require('dotenv').config()
 
 const client = new twitter({
-  consumer_key: 'd5en8vWAQoXLiYKQKAlC28HHo',
-  consumer_secret: 'KbDywwLGgzWNyJazF3waBiiRq7ovuGyPEUakl1sGrQSFW1bx1k',
-  access_token_key: '3097855788-SJ9jZ1DnVujdrG97JX1PygzOZdRmKmvx4c5XNCn',
-  access_token_secret: 'dTfNCFJO80yyNDtSBMeg1ifiS90eRh3jBBJuLejcV6kg2'
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
 const params = {
