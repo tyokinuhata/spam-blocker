50ea326b8d88f43f327c163c8857ec7458477c5d
Kazuki Tobita <kazukiti201@gmail.com>
2018-04-03 04:38:18 +0900

    ブロック機能を実装

--- index.js
+++ index.js
@@ -12,19 +12,45 @@ const search = () => {
 
   const params = {
     q: '仮想通貨に関するビックウェーブ',
-    lang: 'ja'
+    lang: 'ja',
+    locale: 'ja',
+    result_type: 'mixed',
+    count: 100,
   }
 
   client.get('search/tweets', params, (err, tweets, res) => {
     const statuses = tweets.statuses
-    const blocks = []
+    const target = []
 
     for (status of statuses) {
-      blocks.push(status.id)
+      target.push({
+        screen_name: status.user.screen_name,
+        text: status.text
+      })
     }
 
-    fsExtra.writeJSONSync(__dirname + '/blocks.json', blocks)
+    fsExtra.writeJSONSync(__dirname + '/target.json', target)
   })
 }
 
-search()
+const block = () => {
+  const client = new twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  })
+
+  const params = {
+    screen_name: 'TV4DZUthYruyX7U'
+  }
+
+  const target = fsExtra.readJSONSync(__dirname + '/target.json')
+
+  client.post('blocks/create', params, (err, tweets, res) => {
+    console.log(res)
+  })
+}
+
+// search()
+block()
