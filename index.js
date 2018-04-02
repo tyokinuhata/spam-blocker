a719c1f1aacf15aabdfb8ef5f95e35694de46cc0
Kazuki Tobita <kazukiti201@gmail.com>
2018-04-03 03:47:31 +0900

    update search

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const twitter = require('twitter')
+const fsExtra = require('fs-extra')
 require('dotenv').config()
 
-const client = new twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-})
+const search = () => {
+  const client = new twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  })
 
-const params = {
-  q: '仮想通貨に関するビックウェーブ',
-  lang: 'ja'
+  const params = {
+    q: '仮想通貨に関するビックウェーブ',
+    lang: 'ja'
+  }
+
+  client.get('search/tweets', params, (err, tweets, res) => {
+    const statuses = tweets.statuses
+    const blocks = []
+
+    for (status of statuses) {
+      blocks.push(status.id)
+    }
+
+    fsExtra.writeJSONSync(__dirname + '/blocks.json', blocks)
+  })
 }
 
-const blocks = []
-
-client.get('search/tweets', params, (err, tweets, res) => {
-  const statuses = tweets.statuses
-
-  statuses.forEach((val, i) => {
-    blocks.push(val.id)
-  })
-})
+search()
