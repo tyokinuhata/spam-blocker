2d4bd2cb9fd477261733e584c97362fe98fde26b
Kazuki Tobita <kazukiti201@gmail.com>
2018-04-04 09:49:39 +0900

    ファイル分割

--- index.js
+++ index.js
@@ -1,64 +1,9 @@
-const twitter = require('twitter')
-const fsExtra = require('fs-extra')
-require('dotenv').config()
+const search = require(__dirname + '/lib/search.js')
+const block = require(__dirname + '/lib/block.js')
 
-const search = () => {
-  const client = new twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-  })
-
-  const params = {
-    q: process.env.Q,
-    lang: 'ja',
-    locale: 'ja',
-    result_type: 'mixed',
-    count: 100,
-  }
-
-  client.get('search/tweets', params, (err, tweets, res) => {
-    const statuses = tweets.statuses
-    const targets = []
-
-    for (status of statuses) {
-      targets.push(status.user.screen_name)
-      console.log('added ' + status.user.screen_name)
-    }
-
-    fsExtra.writeJSONSync(__dirname + '/targets.json', targets)
-  })
-}
-
-const block = (i) => {
-  const client = new twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-  })
-
-  const targets = fsExtra.readJSONSync(__dirname + '/targets.json')
-
-  if (typeof targets[i] === 'undefined') {
-    console.log('completed')
-    return
-  }
-
-  const params = {
-    screen_name: targets[i]
-  }
-
-  client.post('blocks/create', params, (err, tweets, res) => {
-    if (!err) console.log('blocked @' + targets[i])
-    else console.log(err.message)
-  })
-}
-
-search()
+search.search()
 let i = 0
 setInterval(() => {
-  block(i)
+  block.block(i)
   i++
 }, 5000)
